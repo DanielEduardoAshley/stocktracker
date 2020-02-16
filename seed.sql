cb6fe1e21651321fb9ad73b959585e6b5cfdb210
daniel ashley <danielashley@pursuit.org>
2020-02-16 18:52:28 -0500

    Created and launched seed file to database

--- seed.sql
+++ seed.sql
@@ -1,19 +1,31 @@
-  
+DROP DATABASE IF EXISTS stocktracker;
+CREATE DATABASE stocktracker;
 
-/c DROP IF EXISTS stocktracker
+\c stocktracker;
 
- user{
-     id,
-     firstname VARCHAR,
-     lastname VARCHAR,
+DROP TABLE IF EXISTS users, stock;
+
+ CREATE TABLE users
+ (
+     id SERIAL PRIMARY KEY,
+     users_name VARCHAR,
+     users_encryption VARCHAR,
      email VARCHAR,
-     cash nu
- },
+     sessions_id VARCHAR NULL,
+     cash_remaining INT DEFAULT 5000
+ );
 
- stock{
-     id,
-     user_id REFERENCES (id) user
+CREATE TABLE stock
+(
+     id SERIAL PRIMARY KEY,
+     user_id INT REFERENCES users(id) ON DELETE CASCADE,
      symbol VARCHAR,
-     boughtPrice,
-     quantity,
- },
+     bought_price INT,
+     quantity INT
+);
+
+INSERT INTO users
+VALUES('1', 'dan','1234', 'dan@gmail.com','5678');
+
+INSERT INTO stock
+VALUES('1', '1', 'AAPL', 100, 10);
