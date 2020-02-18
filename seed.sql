dcfe2622f7d656c17bf0e0528f219c50f4299c89
daniel ashley <danielashley@pursuit.org>
2020-02-18 18:48:18 -0500

    Added db connection for database and updated seed.sql file

--- seed.sql
+++ seed.sql
@@ -8,7 +8,9 @@ DROP TABLE IF EXISTS users, stock;
  CREATE TABLE users
  (
      id SERIAL PRIMARY KEY,
-     users_name VARCHAR,
+     first_name VARCHAR,
+     last_name VARCHAR,
+     user_uid VARCHAR,
      users_encryption VARCHAR,
      email VARCHAR,
      sessions_id VARCHAR NULL,
@@ -25,7 +27,7 @@ CREATE TABLE stock
 );
 
 INSERT INTO users
-VALUES('1', 'dan','1234', 'dan@gmail.com','5678');
+VALUES('1', 'dan','ash','5678','1234', 'dan@gmail.com');
 
 INSERT INTO stock
 VALUES('1', '1', 'AAPL', 100, 10);
