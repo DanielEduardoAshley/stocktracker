8193c1c75fd32b3fdb326493307429709a34ebc9
daniel ashley <danielashley@pursuit.org>
2020-02-18 20:52:40 -0500

    Created routes and for users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,2 +1,43 @@
 const express = require('express')
 const userRoutes = express.Router()
+const userServices = require('../services/userServices')
+
+
+
+userRoutes.post('/new', (req, res)=>{
+     const {first_name, last_name, email, token, uid} = req.body
+     userServices.create(first_name, last_name, email, token, uid).catch(err=>{
+         console.log(err)
+     })
+     .then(data=>{
+         res.status(200).send(data)
+     })
+  
+})
+
+
+
+userRoutes.get('/', (req,res)=>{
+      const {id} = req.body
+      userServices.read(id).catch(err=>{
+          console.log(err)
+      })
+      .then(data=>{
+        res.status(200).send(data)
+    })
+})
+
+
+
+userRoutes.put('/update', (req, res)=>{
+    const {first_name, last_name, email, token, uid} = req.body
+    userServices.update(id, first_name, last_name, email, token, uid).catch(err=>{
+        console.log(err)
+    })
+    .then(data=>{
+        res.status(200).send(data)
+    })
+
+
+})
+
