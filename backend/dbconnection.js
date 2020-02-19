9f69fcf2cb1d24c78ab008f62dc2e58aa8a959df
daniel ashley <danielashley@pursuit.org>
2020-02-18 19:17:52 -0500

    Added git ignore folder with node modules, added package.json, created oncify function with closure

---
+++ backend/dbconnection.js
@@ -0,0 +1,14 @@
+const dbconn = ((conn)=>{
+    let db = null
+    const connection=()=>{
+        if(!db){
+            const pg = require('pg-promise')({})
+            db = pg(conn)
+            return db
+        }
+        else{
+            return db
+        }
+    }  
+    return connection 
+})('postgres://localhost/stocktracker')
